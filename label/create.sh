4759c9973e3e62d2b2a82e7da579e18d07b391ce
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2021-08-31 09:13:01 -0400

    Added options

--- label/create.sh
+++ label/create.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-wkhtmltopdf label.html pdf.pdf
+# use this website
+# https://wkhtmltopdf.org/usage/wkhtmltopdf.txt
+
+wkhtmltopdf --page-size A4 --margin-left 10 --margin-right 20 label.html pdf.pdf
