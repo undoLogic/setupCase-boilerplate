6da1a00468bd23759d7d8ffa183aa1790f0e41ba
lathag <latha3@hotmail.com>
2021-08-31 11:32:35 -0400

    set page margin

--- docker/control/label/create.sh
+++ docker/control/label/create.sh
@@ -6,4 +6,4 @@
 #rm pdf.pdf
 #sleep 1
 # we need margins here to calibrate for each printer
-wkhtmltopdf --page-width 102 --page-height 58 label.html pdf3.pdf
+wkhtmltopdf --page-width 102 --page-height 58 --margin-top 3 --margin-right 3 --margin-bottom 3 --margin-left 3 label.html pdf4.pdf
