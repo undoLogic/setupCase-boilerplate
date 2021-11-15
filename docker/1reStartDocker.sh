b339679e6d5f13c01fe9d61d1f0ee73d18469a47
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2021-11-15 12:27:50 -0500

    start docker now accept command line arguments for the filename > Allows to target multiple filenames on larger projects

--- docker/1reStartDocker.sh
+++ docker/1reStartDocker.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-docker-compose down
+if [ -n "$1" ]; then
+  FILENAME=$1
+  # param eixsts
+else
+  # not
+  FILENAME="docker-compose.yml"
+fi
+
+echo docker-compose -f $FILENAME down
 sleep 5
-docker-compose up -d
+docker-compose -f $FILENAME up -d
