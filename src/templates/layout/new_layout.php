2d063e6805fc960a26c7bdad2cd956fcda535a69
lathag <latha3@hotmail.com>
2022-05-03 09:00:24 -0400

    vueStage1

--- src/templates/layout/new_layout.php
+++ src/templates/layout/new_layout.php
@@ -11,10 +11,6 @@ $cakeDescription = 'CakePHP4';
     </title>
     <?= $this->Html->meta('icon') ?>
 
-    <link href=>
-
-
-
     <?= $this->Html->css([
         "https://getbootstrap.com/docs/4.0/examples/dashboard/",
         "https://fonts.googleapis.com/css?family=Raleway:400,700"
@@ -29,6 +25,7 @@ $cakeDescription = 'CakePHP4';
         "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js",
         "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.2/angular.min.js",
         "https://unpkg.com/vue@3.0.2"
+
 
     ]) ; ?>
 
@@ -49,10 +46,10 @@ $cakeDescription = 'CakePHP4';
 
 <?= $this->element('a/header'); ?>
 
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <?= $this->element('a/navigation'); ?>
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+
+        <main class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
 
             <?= $this->Flash->render() ?>
 
