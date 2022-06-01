04e8805ea5c4b1ac683abef386c3ed365168eb7a
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-06-01 13:55:26 -0400

    Cleaning up scripts which were never finalzed and working correctly

--- docker/1reStartDocker.sh
+++ docker/1reStartDocker.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
-
 docker volume prune -f
-
-if [ -n "$1" ]; then
-  FILENAME=$1
-  # param eixsts
-else
-  # not
-  FILENAME="docker-compose.yml"
-fi
-
-docker-compose -f $FILENAME down
-sleep 5
-docker-compose -f $FILENAME up -d
+docker-compose down
+sleep 3
+docker-compose up -d
