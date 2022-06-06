e325d30637a0f7d8258b337739fc9acc8e884e1a
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-06-06 16:27:17 -0400

    Setup scripts

--- docker/control/2install-cake4.sh
+++ docker/control/2install-cake4.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 php composer.phar create-project --prefer-dist cakephp/app /var/www/vhosts/website.com/www/src
+#we need Authentication for our hashing algorithm
+./composer.phar require "cakephp/authentication:^2.0" -d/var/www/vhosts/website.com/www/src
---
+++ docker/control/3importSetupCaseV4.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rsync -av /var/www/vhosts/website.com/www/libraries/cakePHP/4/. /var/www/vhosts/website.com/www/src/.
