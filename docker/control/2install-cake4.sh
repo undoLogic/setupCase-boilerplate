12267777d6a12f89046506e591d714eb02205242
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-06-06 16:43:43 -0400

    Setup scripts

--- docker/control/2install-cake4.sh
+++ docker/control/2install-cake4.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 php composer.phar create-project --prefer-dist cakephp/app /var/www/vhosts/website.com/www/src
+
 #we need Authentication for our hashing algorithm
 ./composer.phar require "cakephp/authentication:^2.0" -d/var/www/vhosts/website.com/www/src
+
+# we do not need these files as we are taking in the parent directory
+rm /var/www/vhosts/website.com/www/src/.gitignore
+rm /var/www/vhosts/website.com/www/src/.gitattributes
