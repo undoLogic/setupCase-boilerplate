262e0dd59242957479c09216cea4415aef43cc78
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-06-09 17:24:16 -0400

    Added cakephp4 files

---
+++ src/templates/Users/begin_reset.php
@@ -0,0 +1,19 @@
+<?= $this->Form->create(); ?>
+    <div class="card">
+        <div class="card-header">
+            <h5>Reset</h5>
+        </div>
+        <div class="card-body">
+            <div class="row">
+
+                <div class="col-lg-12">
+                    <?= $this->Form->control('email', ['class' => 'form-control', 'placeholder' => 'Email']); ?>
+                </div>
+
+                <div class="col-lg-12 text-left m-t-2">
+                    <?= $this->Form->submit('Begin Reset Process', ['class' => 'btn btn-primary']); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+<?= $this->Form->end(); ?>
