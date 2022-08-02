dcd1a328660df036b2c66333f0e3818aca07d547
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-08-02 09:23:28 -0400

    Added latha's new work into the library

--- libraries/cakePHP/4/templates/SetupPages/form_validation.php
+++ libraries/cakePHP/4/templates/SetupPages/form_validation.php
@@ -24,6 +24,13 @@
     <div class="card mt-1 ml-1 mr-1">
         <div class="card-header">
             <h5>Vue2 Form Validation</h5>
+            Required fields:
+            <span v-for="(field,fieldKey) in fields" :id="'key-'+fieldKey">
+                {{fieldKey}} {{field}} <a href="#" @click="removeValidation(fieldKey)">Remove</a><br>
+            </span>
+
+
+
         </div>
         <div class="card-body">
             <form @submit="checkForm" novalidate="true">
@@ -51,29 +58,116 @@
                         <input id="email" v-model="formData.email" type="email" name="email" class="form-control">
 
                     </div>
+                    <div class="col-lg-6">
+                        <label>Single Checkbox</label><br>
+                        <input type="checkbox" id="checkbox" v-model="checked"> Checkbox<br>
+                        <label for="checkbox">Checked True Or False: {{ checked }}</label>
+                    </div>
+
 
                     <!-- Favourite movie -->
                     <div class="col-lg-6">
                         <label for="movie">Favorite Movie</label>
                         <select id="movie" v-model="formData.movie" name="movie" class="form-control">
                             <option>Star Wars</option>
-                            <option>Vanilla Sky</option>
-                            <option>Atomic Blonde</option>
+                            <option>Star Trek</option>
+                        </select>
+                    </div>
+                    <!-- province -- options dropdown -->
+                    <div class="col-lg-6">
+                        <label for="province">Province</label>
+                        <select id="province" v-model="formData.province" name="province" class="form-control">
+
+                            <option v-for="(province, key) in provinces" :value="key">{{province}}</option>
                         </select>
                     </div>
 
-                    <div class="col-lg-12 mt-2">
-                        <input type="submit" value="Submit" class="btn btn-primary">
+                    <!-- radio -->
+                    <div class="col-lg-6">
+                        <label>Radio</label>
+                        <div class = "row ml-1 mr-2 " v-for="shipping in radioOptions" >
+                            <div class="col-md-1" >
+
+                                <input @click="updateShippingToCart(shipping.key)" type="radio" id="shipping.key" :value="shipping.key" v-model="selectedAddress" name="shippingaddress">
+
+                            </div>
+                            <div class="col-md-9 text-left" :class="{greyed: shipping.key === 'create'}">
+                                {{shipping.value}}
+                            </div>
+
+
+
+                        </div>
                     </div>
-                </div><!-- /row -->
 
 
-            </form>
-        </div><!-- /card-body -->
-    </div><!-- /card -->
+                    <div class="col-lg-12">
+                        <label>Multiple Checkbox</label><br/>
+                        <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+                        <label for="jack">Jack</label>
+                        <input type="checkbox" id="john" value="John" v-model="checkedNames">
+                        <label for="john">John</label>
+                        <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+                        <label for="mike">Mike</label>
+                        <br>
+                        <span>Checked names: {{ checkedNames }}</span>
+                    </div>
+
+
+                </div>
+                <div class="col-lg-12 mt-2">
+                    <input type="button" value="Preview" @click="showModal()" class="btn btn-warning">
+
+                    <input type="submit" value="Submit" class="btn btn-primary">
+                </div>
+        </div><!-- /row -->
+
+
+        </form>
+    </div><!-- /card-body -->
+</div><!-- /card -->
+
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Data to be submitted</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Name: {{formData.name}}<br/>
+                Email: {{formData.email}}<br/>
+                movie: {{formData.movie}}<br/>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
 
 
 </div><!-- /app -->
+
+
+
+
+
+
+
+
 
 
 
@@ -97,17 +191,30 @@
     const app = new Vue({
         el: '#app',
         data: {
+            isModalVisible: true,
             errors: [],
             formData: {},
             //FIELDS
             fields: {
                 name: {rule: 'notBlank'},
                 email: {rule: 'notBlank'},
+                province: {rule: 'notBlank'},
                 movie: {rule: 'notEqual', string: 'Star Wars'}
-            }
+            },
+            provinces: {'1':'AB', '2': 'BC'},
+            radioOptions: [{"key":"1", "value": "Yes"} ,{"key": "0", "value": "No"}],
+            selectedAddress: 0,
+            checked: true,
+            checkedNames: ['John', ]
+
         },
         methods: {
-
+            showModal() {
+                $('#exampleModal').modal('show');
+            },
+            removeValidation: function(fieldKey) {
+                this.$delete(this.fields[fieldKey], 'rule');
+            },
             checkForm: function (e) {
                 window.axios.defaults.headers.common['X-CSRF-TOKEN'] = "<?= $csrf; ?>";
                 this.errors = [];
@@ -115,13 +222,11 @@
                 //loop
                 Object.keys(this.fields).forEach(key => {
 
-                    console.log(key);
-                    console.log(this.fields[key]);
-                    console.log(this.fields[key]['rule']);
-
                     let field = key;
                     let rule = this.fields[key]['rule'];
                     let string = this.fields[key]['string'];
+
+                    document.getElementById(field).classList.remove('empty');
 
                     if (rule === 'notBlank') {
                         if (!this.formData[field]) {
@@ -164,6 +269,7 @@
                 }
                 e.preventDefault();
             }
-        }
+        },// end of method
+
     })
 </script>
