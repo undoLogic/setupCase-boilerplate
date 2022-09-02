f7cfac8a6fa7bae8ce4f3458db743146d6ab19a8
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-09-02 11:36:47 -0400

    Improved the caching system - added docs

--- docker/control/caching/SetupCase-Caching.sh
+++ docker/control/caching/SetupCase-Caching.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-#INPUT="hellohowareyou"
-#SUBSTRING=$(echo $INPUT| cut -c 1-3)
-#echo $SUBSTRING
-
-#echo "${TEST:10:15}"
-
 # create the base directory
 # @todo add detection if already exists
 DIR="/var/www/vhosts/website.com/internal"
@@ -19,14 +13,16 @@ fi
 
 echo "Starting to cache modified files"
 while :; do
-  #sleep 1
-  dateIS=$(date +"%M:%S")
+ dateIS=$(date +"%M:%S")
+  DIR="/var/www/vhosts/website.com/www/sourceFiles/"
+  if [ ! -d "$DIR" ]; then
+    echo "$dateIS - dir: 'sourceFiles' is missing - run ./2install-cake4.sh - waiting for 10 seconds ..."
+    sleep 10
+    else
+        find /var/www/vhosts/website.com/www/sourceFiles/ -amin -1 -type f -name '*.php' | while read -r line; do { result=$(echo "$line" | sed "s/www\/sourceFiles/internal/"); cp $line $result; SUBSTRING=$(echo $line| rev | cut -c 1-40 | rev);  echo "${dateIS} - updated: ...${SUBSTRING}"; } done
 
-  # new method
-  #find /var/www/vhosts/website.com/www/sourceFiles/ -amin -0.2 -type f | xargs cp -i /var/www/vhosts/website.com/www/test/
-  find /var/www/vhosts/website.com/www/sourceFiles/ -amin -1 -type f -name '*.php' | while read -r line; do { result=$(echo "$line" | sed "s/www\/sourceFiles/internal/"); cp $line $result; SUBSTRING=$(echo $line| rev | cut -c 1-40 | rev);  echo "${dateIS} - updated: ...${SUBSTRING}"; } done
-
-  # Old method
-  # rsync -av --exclude='.git/' /var/www/vhosts/website.com/www/sourceFiles/. /var/www/vhosts/website.com/internal --delete
-  # trap "echo Exited!; exit;" SIGINT SIGTERM
+    # Old method
+    # rsync -av --exclude='.git/' /var/www/vhosts/website.com/www/sourceFiles/. /var/www/vhosts/website.com/internal --delete
+    # trap "echo Exited!; exit;" SIGINT SIGTERM
+  fi
 done
