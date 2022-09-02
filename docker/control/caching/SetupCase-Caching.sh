6e97e4a9a75d9a31df6cacb9dccb531b64e9f3d2
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-09-01 20:28:07 -0400

    Figuring out best way to copy into the cache -> faster then rsync -> Seems like find with pipe and cp and path renaming is working well

--- docker/control/caching/SetupCase-Caching.sh
+++ docker/control/caching/SetupCase-Caching.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# cp --parents /var/www/vhosts/website.com/www/sourceFiles/src/Controller/AppController.php /var/www/vhosts/website.com/www/test
+# find /var/www/vhosts/website.com/www/sourceFiles/src/Controller/AppController.php -type f | cpio -p -dumv /var/www/vhosts/website.com/www/test/.
+
+# find /var/www/vhosts/website.com/www/sourceFiles/ -amin -30 -type f | sed -e 's/sourceFiles/internal/' |
+
+# find /var/www/vhosts/website.com/www/sourceFiles/ -amin -30 -type f | while read -r line; do { VAR="123"; echo "cp $line " sed -e 's/sourceFiles/internal/';} done
+
+# result=$(echo "$firstString" | sed "s/sourceFiles/internal/")
+# find /var/www/vhosts/website.com/www/sourceFiles/ -amin -30 -type f | while read -r line; do echo $line | sed -e 's/sourceFiles/internal/'; done
+# find /var/www/vhosts/website.com/www/sourceFiles/ -amin -30 -type f \( -name "sourceFiles")
+
+#working
+# find /var/www/vhosts/website.com/www/sourceFiles/ -amin -30 -type f | while read -r line; do { result=$(echo "$line" | sed "s/www\/sourceFiles/internal/"); echo "cp $line $result";} done
+# find /var/www/vhosts/website.com/www/sourceFiles/ -amin -30 -type f | while read -r line; do { result=$(echo "$line" | sed "s/www\/sourceFiles/internal/"); cp $line $result;} done
+# cp /var/www/vhosts/website.com/www/sourceFiles/src/Controller/AppController.php /var/www/vhosts/website.com/internal/src/Controller/AppController.php
+
+
+
+
 _isRunning() {
   ps -ef | grep -v grep | grep "SetupCase-Caching"
 }
@@ -12,12 +31,16 @@ then
 
   while :
   do
+    # new method
+    #find /var/www/vhosts/website.com/www/sourceFiles/ -amin -0.2 -type f | xargs cp -i /var/www/vhosts/website.com/www/test/
+    find /var/www/vhosts/website.com/www/sourceFiles/ -amin -1 -type f | while read -r line; do { result=$(echo "$line" | sed "s/www\/sourceFiles/internal/"); cp $line $result;} done
+
     # @todo create a ctrl+c detection as it get's into a loop and hard to get out
-    rsync -av --exclude='.git/' /var/www/vhosts/website.com/www/sourceFiles/. /var/www/vhosts/website.com/internal --delete
-    trap "echo Exited!; exit;" SIGINT SIGTERM
+    # rsync -av --exclude='.git/' /var/www/vhosts/website.com/www/sourceFiles/. /var/www/vhosts/website.com/internal --delete
+    # trap "echo Exited!; exit;" SIGINT SIGTERM
     date +%T
     #run once a second
-    sleep 1
+    # sleep 1
   done
 else
   echo "Already running - do nothing"
