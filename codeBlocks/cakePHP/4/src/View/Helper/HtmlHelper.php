dec6088b120ff0ea41c73d14403e9f5e425dd4d8
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2022-09-26 16:49:22 -0400

    Added HTML - link auto add the arguments allowing to switch between languages on a page with complex arguments in the url

--- codeBlocks/cakePHP/4/src/View/Helper/HtmlHelper.php
+++ codeBlocks/cakePHP/4/src/View/Helper/HtmlHelper.php
@@ -16,11 +16,25 @@ class HtmlHelper extends Helper\HtmlHelper
     public function link($title, $url = null, array $options = []): string
     {
         if (is_array($url)) {
-            //@todo get language from the request
+
+            //add the language always
+            $req = $this->getView()->getRequest();
+            $lang = $req->getAttribute('lang');
             if (!isset($url['language'])) {
-                $url['language'] = Configure::read('lang');
+                $url['language'] = $lang;
+            }
+
+            //add our arguments to the link in case we are switching languages on a complex url
+            if (!isset($url['pass'])) {
+                $req = $this->getView()->getRequest();
+                $params = $req->getAttribute('params');
+
+                if (isset($params['pass'])) {
+                    $url = $url + $params['pass'];
+                }
             }
         }
+
         return parent::link($title, $url, $options);
     }
 
