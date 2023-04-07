376120b11f2ab624f140e941872f9d044cc59699
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-04-07 13:27:59 -0400

    merging in

--- codeBlocks/cakePHP/4/templates/SetupPages/home.php
+++ codeBlocks/cakePHP/4/templates/SetupPages/home.php
@@ -1,3 +1,12 @@
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.3.slim.min.js" integrity="sha256-ZwqZIVdD3iXNyGHbSYdsmWP//UBokj2FHAxKuSBKDSo=" crossorigin="anonymous"></script>
+<script src="<?= $webroot; ?>modules/prism.js"></script>
+<link rel="stylesheet" href="<?= $webroot; ?>modules/prism.css">
+
+
 <?php $classEach = 'col-12 col-md-6 col-lg-4'; ?>
 
 
