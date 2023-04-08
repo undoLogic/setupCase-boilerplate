90c53d35181339f4cdc449dd192900bf5ac55a8d
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-04-08 06:11:35 -0400

    cleanup

--- build/work_on_main.sh
+++ build/work_on_main.sh
@@ -1,49 +1,44 @@
-# setup on server to make changes
+# setup on server to make changes to this source code
 
+# clone to the server
 git clone https://github.com/undoLogic/setupCase-boilerplate.git .
 
 git fetch origin
 
-# server has different ownership so use this
+# server has different ownership from basedir so use this to block the warning
 git config --global --add safe.directory /home/undoweb/www/testboiler
 
 # see the remote branches (ensure you fetch it first)
-#git branch -a
 git branch -r
-
-# checkout a branch - this did not work
-# git checkout -b origin/2023-04-07-undoWeb
 
 # add without the origin as that was causing an issue
 git checkout -b 2023-04-07-undoWeb
 
+# use merge by default
 git config pull.rebase false
 
+# pull any chnages
 git pull origin 2023-04-07-undoWeb
 
-# Install CakePHP 4 on empty project
+# Install CakePHP 4 on empty project with authentication
 composer create-project --prefer-dist cakephp/app:~4.0 sourceFiles
-
-# Install CakePHP Authentication
 composer require "cakephp/authentication:^2.0" -d sourceFiles
 
-# copy our codeBlocks ontop of CakePHP
+# copy our codeBlocks on top of CakePHP
 rsync -av codeBlocks/cakePHP/4/. sourceFiles/.
 
-# add new files to git - need this for delete as well
+# add new files from cakePHP installation to git - need this for any delete files as well
 git add --all
 
-#add identity
+#add identity so we can push (one time)
 git config --global user.email "support@undologic.com"
 git config --global user.name "undoLogic"
 
+# Commit
 git commit -m 'Installed cakePHP and codeblocks'
 
-# make sure you have setup personal access tokens
-# ensure organization is allowed
+# make sure you have setup personal access tokens (ensure organization is allowed)
 # https://github.com/settings/tokens?type=beta
 
 # push to server
 git push -u origin --all
-
-#enter username
