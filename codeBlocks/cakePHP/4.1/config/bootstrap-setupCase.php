3fd029456f1368dd74a2eaef305d16bf3296931d
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-06-08 21:15:05 -0400

    Docs and setup process

--- codeBlocks/cakePHP/4.1/config/bootstrap-setupCase.php
+++ codeBlocks/cakePHP/4.1/config/bootstrap-setupCase.php
@@ -1,24 +1,23 @@
 <?php
 declare(strict_types=1);
 
-
 use Cake\Core\Configure;
 
-$liveDomains = [
-    'www.livedomain.com',
-];
-$currentDomain = $_SERVER['HTTP_HOST'];
-
-
-if (isset($_SERVER['SERVER_NAME'])) {
-    $serverEnv = null;
-    switch ($_SERVER['SERVER_NAME']) {
-        case 'testboilerplate.undoweb.com':
-            Configure::load('app_setupCase', 'default');
-            break;
-        default:
-            die('Missing environment');
-    }
-}
+//$liveDomains = [
+//    'www.livedomain.com',
+//];
+//$currentDomain = $_SERVER['HTTP_HOST'];
+//
+//
+//if (isset($_SERVER['SERVER_NAME'])) {
+//    $serverEnv = null;
+//    switch ($_SERVER['SERVER_NAME']) {
+//        case 'testboilerplate.undoweb.com':
+//            Configure::load('app_setupCase', 'default');
+//            break;
+//        default:
+//            die('Missing environment');
+//    }
+//}
 
 //dd(get_cfg_var('UNDOWEB.EmailTransport.default.password'));
