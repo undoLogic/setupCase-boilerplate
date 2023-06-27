ba00544043d6969ca96a186c7c1bb997fcd05953
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2023-06-27 11:42:27 -0400

    only small directory to clean with fix line endings, much faster

--- docker/control/fix-windows-line-endings.sh
+++ docker/control/fix-windows-line-endings.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd /var/www/vhosts/website.com/www/sourceFiles/bin && find . -type f -not -path '*/\.git/*' -print0 | xargs -0 dos2unix
+# you should only need to clean this directory
+cd /var/www/vhosts/website.com/www/sourceFiles/vendor/bin && find . -type f -not -path '*/\.git/*' -print0 | xargs -0 dos2unix
