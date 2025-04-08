b69b4a5c1eedc40d3aabef76103ace2134f44c3c
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2025-04-08 03:18:48 -0400

    Remote command is working

--- launchPad/gitclone.ps1
+++ launchPad/gitclone.ps1
@@ -7,17 +7,19 @@ $github_repo = $settings.TESTING.github_repo
 $github_ssh_key_name = $settings.TESTING.github_ssh_key_name
 $copy_src_to_root = $settings.TESTING.copy_src_to_root
 $absolute_path = $settings.TESTING.absolute_path
+$user = $settings.TESTING.user
+$url = $settings.TESTING.url
 
 ########################################################################################### Get Current Branch
 $github_current_branch = git branch --show-current
 if ($github_current_branch -eq "master") {
     $github_current_branch = "master"
 }
+# Force to use branch
+$github_current_branch = 'master'
 
 # (Optional) Output
-Write-Host "Current Git branch: $github_current_branch"
-
-
+Write-Host "Using currnet Git branch: $github_current_branch"
 
 ############################################################################################ Setup connection
 if ($github_use_pat -eq $true) {
@@ -29,27 +31,24 @@ if ($github_use_pat -eq $true) {
 
 ############################################################# Define branch and extract project name from repo
 
-$projectName = $github_repo.Split('/')[-1]
-$remotePath = "~/projects/$projectName"
+#$projectName = $github_repo.Split('/')[-1]
 
-# Remote command to run
+######################################################################################## Remote command to run
 $remoteCommand = @"
 cd $absolute_path &&
 rm -rf $github_current_branch &&
-git clone $gitUrl --branch $github_current_branch --single-branch $absolute_path
+git clone $gitUrl --branch $github_current_branch --single-branch $absolute_path/$github_current_branch
 "@ -replace "`r`n", " "
-
-# SSH command that will be run
-$sshCommand = "ssh user@host '$remoteCommand'"
-
-
 
 ############################################################################################# Copy src to root
 if ($copy_src_to_root -eq $true) {
     $remoteCommand += " && rsync -av --no-perms --omit-dir-times --fake-super $absolute_path/$github_current_branch/ ."
 }
 
-$ABSOLUTE_PATH/$GITHUB_CURRENT_BRANCH/$SRC_FILES_RELATIVE_PATH/ ."
+################################################################################# SSH command that will be run
+$sshCommand = "ssh $user@$url '$remoteCommand'"
 
+#$ABSOLUTE_PATH/$GITHUB_CURRENT_BRANCH/$SRC_FILES_RELATIVE_PATH/ ."
 
 Write-Host $remoteCommand
+#Write-Host $sshCommand
