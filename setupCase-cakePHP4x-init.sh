45c346b51fb974ed12f176f4e7975e9553ddd4ac
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2025-08-24 19:22:18 -0400

    Setting up DEV

--- setupCase-cakePHP4x-init.sh
+++ setupCase-cakePHP4x-init.sh
@@ -9,3 +9,5 @@ composer require "cakephp/authentication:^2.0" -d sourceFiles
 # Copy in the SetupCase base files
 rsync -av --no-perms --omit-dir-times --fake-super codeBlocks/cakePHP/4.x/. sourceFiles/.
 
+#Custom file modifications go here
+
