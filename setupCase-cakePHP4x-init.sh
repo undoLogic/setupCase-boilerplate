8763f5ccca70c97f6c4c06655dcd7adf5485fefe
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2025-08-24 05:38:01 -0400

    New install process

--- setupCase-cakePHP4x-init.sh
+++ setupCase-cakePHP4x-init.sh
@@ -8,3 +8,4 @@ composer require "cakephp/authentication:^2.0" -d sourceFiles
 
 # Copy in the SetupCase base files
 rsync -av --no-perms --omit-dir-times --fake-super codeBlocks/cakePHP/4.x/. sourceFiles/.
+
---
+++ setupCase-github-CREATENEW.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# this will disconnect from the boilerplate and allow to connect this to a fresh github project
+
+# Remove association so boilerplate in github
+rm -rf sourceFiles/.git*
+rm -rf .git*
+
+# change the README
+rm README
