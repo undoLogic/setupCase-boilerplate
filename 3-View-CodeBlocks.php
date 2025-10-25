ba43bda556d03b8afd700ab4ecfe545787929dfc
undoLogic <info@setupcase.com>
2025-10-25 14:49:06 -0400

    Setting up codeBlocks

--- 3-View-CodeBlocks.php
+++ 3-View-CodeBlocks.php
@@ -1,6 +1,8 @@
 <?php
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$domain   = $_SERVER['HTTP_HOST'];
+$path     = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
 
-$url = "http://localhost/sourceFiles/CodeBlocks";
+$url = $protocol . $domain . $path . '/sourceFiles/CodeBlocks';
 header('Location: ' . $url);
-die();
-
+exit;
