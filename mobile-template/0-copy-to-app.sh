a5271a392fcb18920b5e1c7bf34c083c20e52c3c
Sacha-Lewis Dnytruk <sachalewis@undologic.com>
2026-02-03 21:04:08 -0500

    scripts

--- mobile-template/0-copy-to-app.sh
+++ mobile-template/0-copy-to-app.sh
@@ -4,7 +4,11 @@ set -e
 TARGET="../mobile-app"
 
 echo "Applying mobile template…"
-rsync -av . $TARGET/.
+#rsync -av . $TARGET/.
+rsync -av \
+  --exclude=8-Import-changes-from-mobile-app.sh \
+  --exclude=0-copy-to-app.sh \
+  . "$TARGET/"
 
 cd ../mobile-app
 npm install
