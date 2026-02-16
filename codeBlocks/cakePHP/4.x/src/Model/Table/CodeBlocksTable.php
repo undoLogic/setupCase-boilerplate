c03ce0a95190de76a2bcdfcc435f8f7161fb9034
Sacha-Lewis Dnytruk <sachalewis@undologic.com>
2026-02-16 05:07:27 -0500

    updated codeBlocks from recent devJan13 changes

--- codeBlocks/cakePHP/4.x/src/Model/Table/CodeBlocksTable.php
+++ codeBlocks/cakePHP/4.x/src/Model/Table/CodeBlocksTable.php
@@ -10,9 +10,19 @@ class CodeBlocksTable extends Table
 {
     public function initialize(array $config):void
     {
-        $this->addBehavior('Timestamp');
+        parent::initialize($config); // REQUIRED
+
         $this->setTable('code_blocks');
 
+        $this->addBehavior('Timestamp');
+        $this->addBehavior('AuditLog');
+
+       // belongsTo
+        $this->belongsTo('CodeBlockTypes', [
+            'foreignKey' => 'code_block_type_id'
+        ]);
     }
+
+
 
 }// end
